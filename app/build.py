dbfb45f5da263920ada36f70b06fc256a042a5dc
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-03-24 12:29:53 +0900

    fix: follow-imports で問題ないことが確認できたので削除

--- app/build.py
+++ app/build.py
@@ -13,28 +13,13 @@ JOBS = 16
 
 
 def build():
-    package_list = [
-        "enlighten",
-        "selenium.webdriver.support",
-        "openpyxl",
-        "logging.handlers",
-        "coloredlogs",
-        "yaml",
-    ]
-
     build_command = (
-        "poetry run nuitka3 --follow-imports {package_include} --include-package-data=selenium "
+        "poetry run nuitka3 --follow-imports --include-package-data=selenium "
         "--product-name={name} --file-version={version} --product-version={version} "
         "--windows-icon-from-ico={icon_image} --jobs={jobs} "
         "--standalone --onefile --output-dir=build --script-name=app/amazhist.py"
     ).format(
         jobs=JOBS,
-        package_include=" ".join(
-            map(
-                lambda package_name: "--include-package={package}".format(package=package_name),
-                package_list,
-            )
-        ),
         name="amazhist",
         version="0.1.0",
         icon_image="img/icon.png",
